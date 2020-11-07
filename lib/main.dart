6d80f9dceac4ff10074973920b58debf8962ad5c
Kondo Takeo <takeoverjp@gmail.com>
2020-11-07 23:55:11 +0900

    Measure ffi case.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,21 @@
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Native Bridge Performance Test',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Native Bridge Performance Test'),
     );
   }
 }
@@ -30,71 +23,110 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+typedef NativeMemset = Pointer<Void> Function(Pointer<Uint8>, Int32, Uint64);
+typedef Memset = Pointer<Void> Function(Pointer<Uint8>, int, int);
 
-  void _incrementCounter() {
+Pointer<Uint8> intListToArray(List<int> list) {
+  final ptr = allocate<Uint8>(count: list.length);
+  for (var i = 0; i < list.length; i++) {
+    ptr.elementAt(i).value = list[i];
+  }
+  return ptr;
+}
+
+List<int> arrayToIntList(Pointer<Uint8> ptr, int length) {
+  List<int> list = [];
+  for (var i = 0; i < length; i++) {
+    list.add(ptr[i]);
+  }
+  free(ptr);
+  return list;
+}
+class _MyHomePageState extends State<MyHomePage> {
+  static const MethodChannel kChannel = const MethodChannel('xyz.takeoverjp.example/flutter_linux_bridge_perf/method');
+  static const kCount = 1000;
+  static const kBytes = [1000, 10000, 100000];
+  int _counter = 0;
+  DynamicLibrary _libc;
+  Memset _memset;
+
+  void _measureFfiMemset(int count, int byte) {
+    Pointer<Uint8> ptr = allocate(count: byte);
+
+    Stopwatch sw = new Stopwatch();
+    sw.start();
+    for (int i = 0; i < count; i++) {
+      _memset(ptr, 1, byte);
+    }
+    sw.stop();
+    print('ffi: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+
+    free(ptr);
+  }
+
+  void _measureFfiMemsetAndConvert(int count, int byte) {
+    List<int> list = new List(byte);
+    list.fillRange(0, byte, 2);
+
+    Stopwatch sw = new Stopwatch();
+    sw.start();
+    for (int i = 0; i < count; i++) {
+      final ptr = intListToArray(list);
+      _memset(ptr, 1, byte);
+      arrayToIntList(ptr, byte);
+    }
+    sw.stop();
+    print('ffi & covert: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+  }
+
+  void _measureMethodChannelMemset(int count, int byte) {
+    List<int> list = new List(byte);
+    list.fillRange(0, byte, 2);
+
+    Stopwatch sw = new Stopwatch();
+    sw.start();
+    for (int i = 0; i < count; i++) {
+      kChannel.invokeMethod('memset1', list);
+    }
+    sw.stop();
+    print('MethodChannel: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+  }
+
+  void _onClick() {
+    _libc = DynamicLibrary.open('libc.so.6');
+    _memset =
+      _libc.lookup<NativeFunction<NativeMemset>>('memset').asFunction<Memset>();
+
+    for (var byte in kBytes) {
+      _measureFfiMemset(kCount, byte);
+//      _measureFfiMemsetAndConvert(kCount, byte);
+      _measureMethodChannelMemset(kCount, byte);
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Performance Test Start',
+              style: Theme.of(context).textTheme.headline4,
             ),
             Text(
               '$_counter',
@@ -104,10 +136,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _onClick,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
