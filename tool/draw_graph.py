8728c93bed2a94f84a92d005086e0dfe8071e550
Kondo Takeo <takeoverjp@gmail.com>
2020-11-08 21:21:08 +0900

    Update graph legend.

--- tool/draw_graph.py
+++ tool/draw_graph.py
@@ -1,6 +1,9 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Hiragino Maru Gothic Pro', 'Yu Gothic', 'Meirio', 'Takao', 'IPAexGothic', 'IPAPGothic', 'VL PGothic', 'Noto Sans CJK JP']
 
 df = pd.read_csv(sys.argv[1], encoding='UTF8')
 
@@ -11,8 +14,10 @@ if sys.argv[4] == 'log':
   ax.set_xscale('log')
   ax.set_yscale('log')
 
-for label in [('MethodChannelMemset', 'MethodChannel'), \
-  ('ffiMemsetAndConvert', 'FFI and data conversion'), ('ffiMemsetWithListView', 'FFI with ListView'), ('ffiMemset', 'FFI only')]:
+for label in [('MethodChannelMemset', '実装1. MethodChannel'), \
+  ('ffiMemsetAndConvert', '実装2. FFI (Dart-C間でデータを変換)'), \
+  ('ffiMemsetWithListView', '実装3. FFI (asTypedListを使いDart-C間でバッファを共有)'), \
+  ('ffiMemset', '実装4. FFI (C形式でのみバッファを保持)')]:
   series = df[df['type'] == label[0]]
   print(series)
   data_kb = series['dataSize[KB]']
