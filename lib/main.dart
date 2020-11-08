73b66cfaa7cdfc24ef1e242159ad11569e6950c7
Kondo Takeo <takeoverjp@gmail.com>
2020-11-08 11:14:39 +0900

    Measure memset via MethodChannel.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:typed_data';
 import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -48,66 +49,91 @@ List<int> arrayToIntList(Pointer<Uint8> ptr, int length) {
   free(ptr);
   return list;
 }
+
 class _MyHomePageState extends State<MyHomePage> {
-  static const MethodChannel kChannel = const MethodChannel('xyz.takeoverjp.example/flutter_linux_bridge_perf/method');
+  static const MethodChannel kChannel = const MethodChannel(
+      'xyz.takeoverjp.example/flutter_linux_bridge_perf/method');
   static const kCount = 1000;
-  static const kBytes = [1000, 10000, 100000];
   int _counter = 0;
   DynamicLibrary _libc;
   Memset _memset;
 
-  void _measureFfiMemset(int count, int byte) {
+  static int _usToAvgNs(int us) {
+     return (us * (1000 / kCount)).toInt();
+  }
+
+  void _measureFfiMemset(int kb) {
+    final byte = kb * 1024;
     Pointer<Uint8> ptr = allocate(count: byte);
 
     Stopwatch sw = new Stopwatch();
-    sw.start();
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < kCount; i++) {
+      sw.start();
       _memset(ptr, 1, byte);
+      sw.stop();
+
+      for (int idx in [0, byte~/2, byte - 1]) {
+        assert(ptr[idx] == 1);
+        ptr[idx] = 0;
+      }
     }
-    sw.stop();
-    print('ffi: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+    print('ffiMemset, $kb, ${_usToAvgNs(sw.elapsedMicroseconds)}');
 
     free(ptr);
   }
 
-  void _measureFfiMemsetAndConvert(int count, int byte) {
+  void _measureFfiMemsetAndConvert(int kb) {
+    final byte = kb * 1024;
     List<int> list = new List(byte);
     list.fillRange(0, byte, 2);
 
     Stopwatch sw = new Stopwatch();
-    sw.start();
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < kCount; i++) {
+      sw.start();
       final ptr = intListToArray(list);
       _memset(ptr, 1, byte);
-      arrayToIntList(ptr, byte);
+      final result = arrayToIntList(ptr, byte);
+      sw.stop();
+
+      for (int idx in [0, byte~/2, byte - 1]) {
+        assert(result[idx] == 1);
+      }
     }
-    sw.stop();
-    print('ffi & covert: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+    print('ffiMemsetAndConvert, $kb, ${_usToAvgNs(sw.elapsedMicroseconds)}');
   }
 
-  void _measureMethodChannelMemset(int count, int byte) {
-    List<int> list = new List(byte);
+  Future<int> _measureMethodChannelMemset(int kb) async {
+    final byte = kb * 1024;
+    Uint8List list = new Uint8List(byte);
     list.fillRange(0, byte, 2);
 
     Stopwatch sw = new Stopwatch();
-    sw.start();
-    for (int i = 0; i < count; i++) {
-      kChannel.invokeMethod('memset1', list);
+    for (int i = 0; i < kCount; i++) {
+      sw.start();
+      final result = await kChannel.invokeMethod('memset1', list);
+      sw.stop();
+
+      for (int idx in [0, byte~/2, byte - 1]) {
+        assert(result[idx] == 1);
+      }
     }
-    sw.stop();
-    print('MethodChannel: ${byte}, ${sw.elapsedMicroseconds / count}[us]');
+    print('MethodChannelMemset, $kb, ${_usToAvgNs(sw.elapsedMicroseconds)}');
+    return 0;
   }
 
-  void _onClick() {
+  void _onClick() async {
     _libc = DynamicLibrary.open('libc.so.6');
-    _memset =
-      _libc.lookup<NativeFunction<NativeMemset>>('memset').asFunction<Memset>();
+    _memset = _libc
+        .lookup<NativeFunction<NativeMemset>>('memset')
+        .asFunction<Memset>();
 
-    for (var byte in kBytes) {
-      _measureFfiMemset(kCount, byte);
-//      _measureFfiMemsetAndConvert(kCount, byte);
-      _measureMethodChannelMemset(kCount, byte);
+    print("type, dataSize[KB], time[ns]");
+    for (var kb = 1; kb <= 1024; kb *= 2) {
+      _measureFfiMemset(kb);
+      _measureFfiMemsetAndConvert(kb);
+      await _measureMethodChannelMemset(kb);
     }
+    print("done");
 
     setState(() {
       _counter++;
--- linux/my_application.cc
+++ linux/my_application.cc
@@ -10,9 +10,29 @@
 struct _MyApplication {
   GtkApplication parent_instance;
   char** dart_entrypoint_arguments;
+  FlMethodChannel* method_channel;
 };
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
+
+static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call,
+                           gpointer user_data) {
+  g_autoptr(FlMethodResponse) response = nullptr;
+
+  const gchar* method = fl_method_call_get_name(method_call);
+  FlValue* args = fl_method_call_get_args(method_call);
+
+  if (strcmp(method, "memset1") == 0) {
+    const size_t length = fl_value_get_length(args);
+    const uint8_t* list = fl_value_get_uint8_list(args);
+    memset(const_cast<uint8_t* >(list), 1, length);
+    response = FL_METHOD_RESPONSE(fl_method_success_response_new(args));
+  } else {
+    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
+  }
+
+  fl_method_call_respond(method_call, response, nullptr);
+}
 
 // Implements GApplication::activate.
 static void my_application_activate(GApplication* application) {
@@ -29,22 +49,21 @@ static void my_application_activate(GApplication* application) {
   // if future cases occur).
   gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
-  GdkScreen *screen = gtk_window_get_screen(window);
+  GdkScreen* screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen)) {
-     const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-     if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-       use_header_bar = FALSE;
-     }
+    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
+    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
+      use_header_bar = FALSE;
+    }
   }
 #endif
   if (use_header_bar) {
-    GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_header_bar_set_title(header_bar, "flutter_linux_bridge_perf");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  }
-  else {
+  } else {
     gtk_window_set_title(window, "flutter_linux_bridge_perf");
   }
 
@@ -52,7 +71,8 @@ static void my_application_activate(GApplication* application) {
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
-  fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
+  fl_dart_project_set_dart_entrypoint_arguments(
+      project, self->dart_entrypoint_arguments);
 
   FlView* view = fl_view_new(project);
   gtk_widget_show(GTK_WIDGET(view));
@@ -61,19 +81,30 @@ static void my_application_activate(GApplication* application) {
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
   gtk_widget_grab_focus(GTK_WIDGET(view));
+
+  FlBinaryMessenger* messenger =
+      fl_engine_get_binary_messenger(fl_view_get_engine(view));
+  g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
+  self->method_channel = fl_method_channel_new(
+      messenger, "xyz.takeoverjp.example/flutter_linux_bridge_perf/method",
+      FL_METHOD_CODEC(codec));
+  fl_method_channel_set_method_call_handler(self->method_channel, method_call_cb,
+                                            nullptr, nullptr);
 }
 
 // Implements GApplication::local_command_line.
-static gboolean my_application_local_command_line(GApplication* application, gchar ***arguments, int *exit_status) {
+static gboolean my_application_local_command_line(GApplication* application,
+                                                  gchar*** arguments,
+                                                  int* exit_status) {
   MyApplication* self = MY_APPLICATION(application);
   // Strip out the first argument as it is the binary name.
   self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
 
   g_autoptr(GError) error = nullptr;
   if (!g_application_register(application, nullptr, &error)) {
-     g_warning("Failed to register: %s", error->message);
-     *exit_status = 1;
-     return TRUE;
+    g_warning("Failed to register: %s", error->message);
+    *exit_status = 1;
+    return TRUE;
   }
 
   g_application_activate(application);
@@ -83,22 +114,23 @@ static gboolean my_application_local_command_line(GApplication* application, gch
 }
 
 // Implements GObject::dispose.
-static void my_application_dispose(GObject *object) {
+static void my_application_dispose(GObject* object) {
   MyApplication* self = MY_APPLICATION(object);
+  g_clear_object(&self->method_channel);
   g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
   G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
 }
 
 static void my_application_class_init(MyApplicationClass* klass) {
   G_APPLICATION_CLASS(klass)->activate = my_application_activate;
-  G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
+  G_APPLICATION_CLASS(klass)->local_command_line =
+      my_application_local_command_line;
   G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
 }
 
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  return MY_APPLICATION(g_object_new(my_application_get_type(),
-                                     "application-id", APPLICATION_ID,
-                                     nullptr));
+  return MY_APPLICATION(g_object_new(
+      my_application_get_type(), "application-id", APPLICATION_ID, nullptr));
 }
